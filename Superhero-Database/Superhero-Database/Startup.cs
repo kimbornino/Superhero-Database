05ff9cf650270ac785a3650ffefbd3934d0691ef
Kimbornino <kimbornino@gmail.com>
2019-03-13 08:20:21 -0500

    Initial commit of files

---
+++ Superhero-Database/Superhero-Database/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Superhero_Database.Startup))]
+namespace Superhero_Database
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
